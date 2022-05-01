01bccf8ff529fbc8a243b29e0aad7e40872ddd08
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-05-02 00:31:53 +0300

    first commit

---
+++ program.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(){
+	printf("Hello!");
+	
+	
+	int a = 5,  b = 10;
+	float d = 6.56;
+	double f = 67.789;
+	
+	printf("\n%d\n",a);
+	
+	if(a == b){
+		printf("esittir");
+	}
+	else{
+		printf("esitdegildir");
+	}
+	
+	
+	return 0;
+}
